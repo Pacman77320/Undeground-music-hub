946bd897ec7ba3d88ef794dffa328a38f84b2e52
Pacman77320 <godink77@gmain.com>
2025-06-24 18:05:26 +0200

    Correction de la base URL pour GitHub Pages et re-build

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // AJOUTEZ CETTE LIGNE :
+  base: '/Underground-music-hub/', // Assurez-vous que c'est le nom exact de votre dépôt GitHub
 })
+
